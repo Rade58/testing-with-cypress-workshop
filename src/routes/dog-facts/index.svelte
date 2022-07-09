ed1b318a1d51e4ef265fb52ca7b5c644535ce964
Rade58 <bajic.rade2@gmail.com>
2022-07-09 18:31:16 +0200

    🦉

---
+++ src/routes/dog-facts/index.svelte
@@ -0,0 +1,30 @@
+<!-- DON'T FORGET THAT THIS script TAG NEEDS
+TO HAVE context OF "module" -->
+<script context="module" lang="ts">
+  import type {Load} from '@sveltejs/kit'
+
+  export const load:Load<any, {numberOfFacts: number; random: string}> = async (loadEvent) => {
+
+    const {params,url: {searchParams}} = loadEvent
+    
+
+    const amount = params["amount"]
+    const againAmount = searchParams.get("amount")
+    
+    const amountNum = againAmount? +againAmount:3
+
+
+    // SENDING SOME QUERY PARMAS DATA AS PROPS
+
+    return {
+      props: {
+        // THESE ARE PROPS AS YOU SEE
+        numberOfFacts: amountNum,
+        // I ADDED SOME RANDOM PROP JUST TO PLAY AROUND
+        random: "Shiba inu"
+      }
+    }
+
+  }
+
+</script>
