778a26d1d4aac7b256f509b834da4fd579b5ad8f
Rade58 <bajic.rade2@gmail.com>
2022-07-11 22:06:35 +0200

    🦉

---
+++ src/routes/secret-menu/index.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+  import type {Load} from '@sveltejs/kit'
+  export const ssr = false;
+
+  export const load: Load = async({fetch}) => {
+
+    const {data}: SecretMenuItemAPIResponseType = 
+      await fetch("/secret-menu/api")
+      .then(res => {
+        return res.json()
+      })
+
+
+    return {
+      props: {
+        data
+      }
+    }
+  }
+</script>
+
+
+<script lang="ts">
+
+  export let data: SecretMenuItemType[] = []
+
+</script>
