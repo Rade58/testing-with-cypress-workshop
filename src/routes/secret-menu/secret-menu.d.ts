40cb5e7a25678a5259638cc1ebae1b7b7f13a4e2
Rade58 <bajic.rade2@gmail.com>
2022-07-11 21:11:08 +0200

    🦉

---
+++ src/routes/secret-menu/secret-menu.d.ts
@@ -0,0 +1,17 @@
+type SecretMenuItemType = {
+	name: string;
+	whereToOrder: string;
+	sourceUrl: string;
+	description: string;
+	imageUrl: string;
+	availability: string;
+	secret: string;
+	ingredients: string;
+	popularity: number;
+	price: string;
+	howToOrder: string;
+};
+
+type SecretMenuItemAPIResponseType = {
+	dataa: SecretMenuItemAPIResponseType[];
+};
