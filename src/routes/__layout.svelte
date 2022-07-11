001b000357d5cfe027a496d154303949e8476e5f
Rade58 <bajic.rade2@gmail.com>
2022-07-11 19:18:03 +0200

    🦉

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -88,11 +88,11 @@
   <div class="flex-none mr-8">
     <ul class="menu menu-horizontal p-0">
 
-			{#each navs as nav}
+			{#each navs as nav, i}
 
       <li tabindex="0">
         <a>
-          more apps
+          {i + 1}. more apps
           <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul class="p-2 bg-base-100">
