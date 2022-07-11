fc1d431b61c4d892c6bfd68f8ed91b2987eca3e9
Rade58 <bajic.rade2@gmail.com>
2022-07-11 21:19:35 +0200

    🦉

--- src/routes/secret-menu/api.ts
+++ src/routes/secret-menu/api.ts
@@ -1,11 +1,10 @@
-import type {RequestHandler} from '@sveltejs/kit'
-import data from './secret-menu.json'
+import type { RequestHandler } from '@sveltejs/kit';
+import data from './secret-menu.json';
 
-export const get: RequestHandler = async () => {
-
-  return {
-    body: {
-      data
-    }
-  }
-}
+export const get: RequestHandler<any, SecretMenuItemAPIResponseType> = async () => {
+	return {
+		body: {
+			data: data as SecretMenuItemAPIResponseType
+		}
+	};
+};
