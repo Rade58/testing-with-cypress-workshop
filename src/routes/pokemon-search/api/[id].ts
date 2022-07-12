b702118742d30cf8d6b0629089f8498be1931dd2
Rade58 <bajic.rade2@gmail.com>
2022-07-12 21:53:02 +0200

    🦉

---
+++ src/routes/pokemon-search/api/[id].ts
@@ -0,0 +1,22 @@
+// THISS ENDPOINT WILL SERVE ONE POKEMON BASED ON ID YOU PROVIDED AS
+// PART OF THE ROUTE
+import type { RequestHandler } from '@sveltejs/kit';
+import data from '../pokemon.json';
+
+export const get: RequestHandler = async ({ params }) => {
+	const { id } = params;
+
+	for (const pokemon of data) {
+		if (pokemon.id === +id) {
+			return {
+				body: {
+					pokemon
+				}
+			};
+		}
+	}
+
+	return {
+		status: 404
+	};
+};
