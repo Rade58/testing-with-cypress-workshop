99b48b703a5440cd21ed35da450abab05fcad473
Rade58 <bajic.rade2@gmail.com>
2022-07-15 02:44:53 +0200

    🦉

--- src/routes/echo-chamber/api/_respond.ts
+++ src/routes/echo-chamber/api/_respond.ts
@@ -1,25 +1,31 @@
-// THIS IS GOING TO BE A HELPER WHN HANDLING JWT
+// THIS IS GOING TO BE A HELPER FOR HANDLING JWT
 // I GUESS THAT IS WHY IT IS UNDERSCORED FILE
+import type { User } from '@prisma/client';
 import { encodeToken } from '$util/jwt';
 
-export function respond(body: any) {
-	if (body.errors) {
-		return { status: 401, body };
+// IDEA IS THAT WE PASS USER RECORD (FROM DATABASE)
+// HERE AS AN ARGUMENT
+// AND WE MAKE jwt COOKIE ENTRY FROM THE VALUES OF USER (FROM email AND id)
+
+export function respond(userData: { errors?: any; user?: User }) {
+	if (userData.errors || !userData.user) {
+		return { status: 401, userData };
 	}
 
 	// WE WILL CREATE BASE64 FROM THIS DATA
 
-	const { id, email } = body.user;
+	const { id, email } = userData.user;
 	const json = JSON.stringify({ id, email });
-	const base64Value = encodeURI(json);
+	const base64Value = encodeToken(json);
 
 	// THIS IS A REDIRECT
+	// AND WE MAKE jwt COOKIE ENTRY
 	return {
 		status: 302,
 		headers: {
 			'set-cookie': `jwt=${base64Value}; Path=/; HttpOnly`,
 			Location: '/echo-chamber/posts'
 		},
-		body
+		userData
 	};
 }
---
+++ src/routes/echo-chamber/api/sign-in.ts
@@ -0,0 +1,76 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import type { User } from '@prisma/client';
+import prisma from '$lib/prisma';
+import { respond } from './_respond';
+
+export const post: RequestHandler = async (event) => {
+	// MAIN IDEA IS IS USER IS REALLY SIGNED IN
+
+	const { body } = event.request;
+
+	let email: string;
+	let password: string;
+
+	if (!body) {
+		return {
+			status: 401
+		};
+	}
+
+	if (body instanceof ReadableStream<Uint8Array>) {
+		const reader = body.getReader();
+
+		const uint8Arr = (await reader.read()).value;
+
+		if (!uint8Arr) {
+			return {
+				status: 401
+			};
+		}
+
+		const data: { email: string; password: string } = JSON.parse(
+			Buffer.from(uint8Arr).toString('utf-8')
+		);
+
+		email = data.email;
+		password = data.password;
+	} else if (typeof body === 'string') {
+		const parsedBody: { email: string; password: string } = JSON.parse(body);
+
+		email = parsedBody.email;
+		password = parsedBody.password;
+	} else {
+		// eslint-disable-next-line
+		// @ts-ignore
+		email = body.get('email');
+		// eslint-disable-next-line
+		// @ts-ignore
+		password = body.get('password');
+	}
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	});
+
+	if (!user) {
+		return {
+			headers: {
+				Location: '/echo-chamber/sign-in?error=No+such+user+exists.'
+			},
+			status: 303
+		};
+	}
+
+	if (user.password !== password) {
+		return {
+			headers: {
+				Location: `/echo-chamber/sign-in?error=Incorrect+password`
+			},
+			status: 303
+		};
+	}
+
+	return respond({ user });
+};
