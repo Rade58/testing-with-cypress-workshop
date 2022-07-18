4e581c9e679f6b9960ae7ddc2d3b43171d365e2f
Rade58 <bajic.rade2@gmail.com>
2022-07-18 21:26:27 +0200

    🦉

--- cypress/e2e/3-foo/01-foo.cy.js
+++ cypress/e2e/3-foo/01-foo.cy.js
@@ -1,24 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Foo test', () => {
-	// WE ARE GOING TO THE PAGE IN OUR PROJECT
 	it('goes to the the localhost of our app; on sign in page', () => {
 		cy.visit('http://localhost:3000/echo-chamber/sign-in');
 	});
-
-	// HERE YOU GO WE USED HERE AN ATTRIBUTE SELECTOR
-	// WE ARE SEARCHING FOR FORM ELEMENT
-
-	// FOR THE FIRST ELEMENT FOUND WE ARE THEN CHECKING IF
-	// THAT ELEMENT HAS SOME ATTRIBUTE
 
 	it('form should have method "POST" on it', () => {
 		cy.get('[data-test="sign-in-form"]').should('have.attr', 'method', 'post');
 	});
 
-	// NOW WE GO TO SOME OOTHER PAGE
-
-	it('fgoess to the signup page', () => {
+	it('goes to the signup page', () => {
 		cy.visit('http://localhost:3000/echo-chamber/sign-up');
+	});
+
+	// HERE YOU GO
+
+	it('form has label with text: "Password"', () => {
+		cy.get('form label:nth-of-type(2)').contains('Password');
 	});
 });
