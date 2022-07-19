5b947c34807e2fb485680762fe1beb335d04d5f0
Rade58 <bajic.rade2@gmail.com>
2022-07-19 18:07:07 +0200

    🦉

--- cypress/e2e/01-first/1-first-spec.cy.js
+++ cypress/e2e/01-first/1-first-spec.cy.js
@@ -1,37 +1,19 @@
 /// <reference types="cypress" />
 
 describe('Create New Item', () => {
-	// FIRST LETS VISIT THE CORRECT PAGE
-	// BUT LET DEFINE THAT WE WANT TO DO THAT
-	// BEFORE EACH TEST IN THIS DESCRIBE BLOCK
 	beforeEach(() => {
 		cy.visit('http://localhost:3000/crypto-pooper');
 	});
 
-	// LETS MAKE A SIMPLE ASSERTION
+	// THIS HANDLER CAN HAVE ARGUMENT done
+	it('should fail if user clickkss on disabled button', (done) => {
+		cy.get('[data-test=add-coin][disabled]').click();
 
-	it('should have a form', () => {
-		cy.get('form').should('exist');
-	});
-
-	// WE NEED TO UNDERSTND WHEN CYPRESS SUCCEDS AND WHEN IT FAILS
-	// FOR EXAMPLE JEST WILL FAIL IF THERE IS NO ASSERTIONS
-	// BUT NOTE THAT THIS IS NOT THE CSE WITH CYPRESS
-
-	// CYPRESS WILLL SUCCED EVEN YOU JUST VISITED PAGE
-	// OR YOU JUST FIND AN ELEMENT
-
-	// SO FAR I KNOW THAT CYPRESS WILL FAIL IF YOU MAK A
-	// WRONG ASSERTION OR IF ELEMENT CAN'T BE FOUND
-
-	// THIS ONE WILL FAIL
-
-	it(' should have a special "formmmmm" element', () => {
-		cy.get('formmmmmm');
-	});
-	// AND THIS ONE WILL FAIL
-
-	it('form shouldn`t exist', () => {
-		cy.get('form').should('not.exist');
+		// NOW WE USE HANDLER
+		cy.once('fail', (err) => {
+			expect(err.message).to.include('this element is `disabled`');
+			// BECAUSE TEST IS ASYNCHRONOUS YOU NEED TO USE done
+			done();
+		});
 	});
 });
