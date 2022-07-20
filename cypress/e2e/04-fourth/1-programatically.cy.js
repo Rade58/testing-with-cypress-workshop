833a07bdbf132a1ccc6257f3294aea5e30e78bcf
Rade58 <bajic.rade2@gmail.com>
2022-07-21 01:06:55 +0200

    🦉

---
+++ cypress/e2e/04-fourth/1-programatically.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+// WE ARE STILL TESTING THIS PART OF THE APP:
+//        /secret-menu
+
+describe('Secret menu items', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/secret-menu');
+  });
+
+  it('', () => {});
+});
