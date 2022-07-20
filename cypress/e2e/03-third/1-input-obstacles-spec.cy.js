455dcfa9462d06784e3d634445ce534d78499fd0
Rade58 <bajic.rade2@gmail.com>
2022-07-20 21:21:47 +0200

    🦉

--- cypress/e2e/03-third/1-input-obstacles-spec.cy.js
+++ cypress/e2e/03-third/1-input-obstacles-spec.cy.js
@@ -1,1 +1,12 @@
 /// <reference types="cypress" />
+
+describe('Input obstacles', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/obstacle-course');
+  });
+
+  it('should input text into the input field', () => {
+    //
+    //
+  });
+});
