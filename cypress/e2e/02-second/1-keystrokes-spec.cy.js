02e43a23e35b85098b0aaaf3f60275308f029a0c
Rade58 <bajic.rade2@gmail.com>
2022-07-20 03:47:27 +0200

    🦉

--- cypress/e2e/02-second/1-keystrokes-spec.cy.js
+++ cypress/e2e/02-second/1-keystrokes-spec.cy.js
@@ -1,5 +1,30 @@
 /// <reference types="cypress" />
 
-it('', () => {
+it('we submit a form with pressing enter', () => {
   cy.visit('http://localhost:3000/crypto-pooper');
+
+  cy.wait(200);
+
+  // IN THIS CASE IN MY APP, PRESSING ENTER WOULD MEAN
+  // SUBMITTING A FORM
+
+  cy.get('[data-test=new-coin-input]').type('Dogebonk{enter}', {
+    delay: 200,
+    timeout: 300
+  });
+
+  cy.contains('Dogebonk').should('exist');
+});
+
+// OR LETS USE .submit METHOD
+
+it('sumbits the form again', () => {
+  cy.get('[data-test=new-coin-input]').type('Shibonk', {
+    delay: 200,
+    timeout: 300
+  });
+
+  cy.get('form').submit();
+
+  cy.contains('Shibonk').should('exist');
 });
