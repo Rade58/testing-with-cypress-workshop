b6a0877a06608d2d82821648b20e46da75d56e81
Rade58 <bajic.rade2@gmail.com>
2022-07-20 20:53:29 +0200

    🦉

---
+++ cypress/e2e/02-second/2-other-inputs-spec.cy.js
@@ -0,0 +1,59 @@
+/// <reference types="cypress" />
+
+describe('Secret Menu Stuff', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/secret-menu');
+
+    // ALIASING RANGE INPUT AND SELECT ELEMENT
+
+    cy.get('#minimum-rating-visibility').as('ranger');
+
+    cy.get('#restaurant-visibility-filter').as('filterSelect');
+  });
+
+  it('should set the range and verify it', () => {
+    // THIS IS HOW YOU WOULD MAKE CTION ON RANGE LEMENT
+    cy.get('@ranger').invoke('val', 6).trigger('input');
+
+    // ASSERTION
+    cy.get('@ranger').should('have.value', 6);
+  });
+
+  it('should check the checkbox and verify it', () => {
+    cy.get('#show-description').as('description-check');
+
+    // I HAVE PROBLEM IN MY UI
+    // I DON'T KNOW IF THAT IS BECAUSE I'M USING DAISY UI LIBRARY
+    // SO INSTEAD OF USING .check
+    // WITH I COULDN'T CRETE ACTION
+    // I AM USING .click
+    // I THINK THE PROBLEM IS WITH checked ATTRIBUTE THAT ISN'T
+    // BEING SHOWN IN DOM ON THE CHECKBOX AT ALL
+    // SO I THINK THAT UI LIBRARY IS UISNG SOME OTHER LOGIC
+
+    // cy.wait(1000);
+    // cy.get('@description-check').check({
+    // waitForAnimations: true,
+    // delay: 1000,
+    // timeout: 1000
+    // });
+    // cy.wait(1000);
+    //
+    // cy.get('@description-check').should('be.checked');
+    //
+    // cy.get('@description-check').should('have.attr', 'checked', false);
+
+    // THIS IS WHAT WORKED
+
+    cy.get('@description-check').click();
+
+    cy.get('@description-check').should('not.be.checked');
+
+    // TRYING AGAIN
+    cy.wait(1000);
+
+    cy.get('@description-check').click();
+
+    cy.get('@description-check').should('be.checked');
+  });
+});
