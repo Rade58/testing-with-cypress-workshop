c1eb627866e58a931c23c480970c99950af4663b
Rade58 <bajic.rade2@gmail.com>
2022-07-22 01:45:18 +0200

    🦉

--- cypress/e2e/05-fifth/3-tasks-spec.cy.js
+++ cypress/e2e/05-fifth/3-tasks-spec.cy.js
@@ -14,6 +14,27 @@ describe('Sign Up', () => {
   });
 
   it('should successfully create a user when entering an email and a password', () => {
+    // Sign Up
     cy.visit('http://localhost:3000/echo-chamber/sign-up');
+
+    cy.get('[data-test=sign-up-email]').type(user.email, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-up-password]').type(user.password, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-up-submit]').click();
+    // Sign In
+
+    cy.visit('http://localhost:3000/echo-chamber/sign-in');
+
+    cy.get('[data-test=sign-in-email]').type(user.email, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-in-password]').type(user.password, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-in-submit]').click();
+
+    cy.wait(2000);
+    // ASSERTIONS
+
+    cy.location('pathname').should('contain', '/echo-chamber/posts');
+
+    cy.get('[data-test="current-user"] > .btn-ghost').click();
+
+    cy.contains(`Profile ${user.email}`);
   });
 });
