450276af786df4bfe4267952be05289ff37b1f97
Rade58 <bajic.rade2@gmail.com>
2022-07-21 02:11:46 +0200

    🦉

--- cypress/e2e/04-fourth/1-programatically.cy.js
+++ cypress/e2e/04-fourth/1-programatically.cy.js
@@ -3,10 +3,68 @@
 // WE ARE STILL TESTING THIS PART OF THE APP:
 //        /secret-menu
 
+// I NEED THIS SINCE I'M GOING TO LOOP OVER THINGS
+
+const restaurants = [
+  'Chick-fil-A',
+  'McDonalds',
+  'In-N-Out',
+  'KFC',
+  'Jack In The Box',
+  'Jamba Juice',
+  'Starbucks',
+  'Dairy Queen',
+  'Burger King',
+  'Chipotle',
+  'Taco Bell',
+  'Five Guys',
+  'Sonic',
+  'Subway',
+  'Panera Bread'
+];
+
+const properties = [
+  'name',
+  'whereToOrder',
+  'description',
+  'secret',
+  'ingredients',
+  'popularity',
+  'price',
+  'howToOrder'
+];
+
+const ratings = [1, 2, 3, 4, 5, 6, 7];
+
+//
 describe('Secret menu items', () => {
-  beforeEach(() => {
+  // beforeEach(() => {
+  // });
+
+  it('visit the secret menu', () => {
     cy.visit('http://localhost:3000/secret-menu');
   });
 
-  it('', () => {});
+  it('title should be on the page', () => {
+    cy.get('h1').should('contain', 'Secret Menu Items');
+  });
+
+  // WE WILL LOOP AND TEST IT PROGRAMATICALLY
+  // WE ARE GENEATING TEST IN EVERY LOOP ITTERATION
+
+  for (const property of properties) {
+    it(`should have colummn for property ${property}`, () => {
+      cy.get(`#${property}-column`);
+    });
+
+    it(`should have a column with input for showing the ${property} column`, () => {
+      cy.get(`#show-${property}`);
+    });
+
+    it(`should hide the column when the checkbox is unchecked`, () => {
+      cy.get(`#show-${property}`).click();
+      cy.wait(400);
+      cy.get(`#${property}-column`).should('have.class', 'hidden');
+    });
+  }
 });
