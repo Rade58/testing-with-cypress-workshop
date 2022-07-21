1f1cf3a6685ce414b09023fcc8fd40d1be06ac3f
Rade58 <bajic.rade2@gmail.com>
2022-07-21 04:04:59 +0200

    🦉

--- cypress/e2e/05-fifth/1-path-tryout-spec.cy.js
+++ cypress/e2e/05-fifth/1-path-tryout-spec.cy.js
@@ -3,7 +3,27 @@
 // WE WILL DEAL A BIT WITH /echo-chamber PAGE
 
 describe('echo chamber tryout', () => {
-  it('visits the echo chamber', () => {
-    cy.visit('http://localhost:3000/echo-chmber');
+  /*   it('visits the echo chamber', () => {
+    cy.visit('http://localhost:3000/echo-chamber');
+  });
+
+ */ beforeEach(() => {
+    cy.visit('http://localhost:3000/echo-chamber');
+
+    cy.get('.dropdown > .btn').first().as('dropdown');
+  });
+
+  it('should have a title of the application in the window', () => {
+    cy.title().should('contain', 'Echo Chamber');
+  });
+
+  it("should navigate to /aign-in when you click on 'Sign In Button'", () => {
+    cy.get('@dropdown').click();
+
+    cy.wait(1000);
+
+    cy.get('[data-test=sign-in]').click();
+
+    cy.location('pathname').should('contain', 'sign-in');
   });
 });
