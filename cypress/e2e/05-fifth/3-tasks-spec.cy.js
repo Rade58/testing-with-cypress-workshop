f2c1370550ce9f674e4255ac330b099daf1a9d5a
Rade58 <bajic.rade2@gmail.com>
2022-07-22 02:01:23 +0200

    🦉

--- cypress/e2e/05-fifth/3-tasks-spec.cy.js
+++ cypress/e2e/05-fifth/3-tasks-spec.cy.js
@@ -38,3 +38,23 @@ describe('Sign Up', () => {
     cy.contains(`Profile ${user.email}`);
   });
 });
+
+describe('Sign In (Failiure Mode)', () => {
+  beforeEach(() => {
+    cy.task('reset');
+    cy.visit('http://localhost:3000/echo-chamber/sign-in');
+  });
+
+  it('should sign in with an existing user', () => {
+    cy.get('[data-test=sign-in-email]').type(user.email, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-in-password]').type(user.password, { delay: 100, timeout: 100 });
+    cy.get('[data-test=sign-in-submit]').click();
+
+    cy.location('pathname').should('contain', '/echo-chamber/sign-in');
+    cy.get('[data-test="current-user"] > .btn-ghost').should('not.exist');
+
+    cy.contains('Error! No such user exists.');
+
+    cy.contains(`Profile ${user.email}`).should('not.exist');
+  });
+});
--- prisma/seed.cjs
+++ prisma/seed.cjs
@@ -2,70 +2,92 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-	const deletePosts = prisma.post.deleteMany({});
-	const deleteUsers = prisma.user.deleteMany({});
+  const deletePosts = prisma.post.deleteMany({});
+  const deleteUsers = prisma.user.deleteMany({});
 
-	await prisma.$transaction([deletePosts, deleteUsers]);
+  await prisma.$transaction([deletePosts, deleteUsers]);
 
-	// CEATE OR UPDATE TWO RANDOM USERS
-	// AND ADD THEIR POSTS
+  // CEATE OR UPDATE TWO RANDOM USERS
+  // AND ADD THEIR POSTS
 
-	const first = await prisma.user.upsert({
-		where: {
-			email: 'foobar@example.com'
-		},
-		update: {},
-		create: {
-			email: 'foobar@example.com',
-			password: 'passwordone1',
-			posts: {
-				create: [
-					{
-						content: 'Shiba Inu is some kind of dog.'
-					},
-					{
-						content: 'My coins go num num!'
-					}
-				]
-			}
-		}
-	});
+  const first = await prisma.user.upsert({
+    where: {
+      email: 'foobar@example.com'
+    },
+    update: {},
+    create: {
+      email: 'foobar@example.com',
+      password: 'passwordone1',
+      posts: {
+        create: [
+          {
+            content: 'Shiba Inu is some kind of dog.'
+          },
+          {
+            content: 'My coins go num num!'
+          }
+        ]
+      }
+    }
+  });
 
-	const second = await prisma.user.upsert({
-		where: {
-			email: 'bazqux@example.com'
-		},
-		update: {},
-		create: {
-			email: 'bazqux@example.com',
-			password: 'passwordtwo2',
-			posts: {
-				create: [
-					{
-						content: 'Akita Inu is most beautiful dog.'
-					},
-					{
-						content: 'Aquaman making bubbles and stuff.'
-					}
-				]
-			}
-		}
-	});
+  const second = await prisma.user.upsert({
+    where: {
+      email: 'bazqux@example.com'
+    },
+    update: {},
+    create: {
+      email: 'bazqux@example.com',
+      password: 'passwordtwo2',
+      posts: {
+        create: [
+          {
+            content: 'Akita Inu is most beautiful dog.'
+          },
+          {
+            content: 'Aquaman making bubbles and stuff.'
+          }
+        ]
+      }
+    }
+  });
 
-	return Promise.all([first, second]);
+  const third = await prisma.user.upsert({
+    where: {
+      email: 'bazqux@example.com'
+    },
+    update: {},
+    create: {
+      email: 'foobar@example.com',
+      password: 'mydaughtersbirthday',
+
+      posts: {
+        create: [
+          {
+            content: 'Akita Inu is most beautiful dog.'
+          },
+          {
+            content: 'Aquaman making bubbles and stuff.'
+          }
+        ]
+      }
+    }
+  });
+
+  return Promise.all([first, second]);
 }
 
 if (require.main === module) {
-	main();
+  main();
 }
 
 module.exports = async () => {
-	main()
-		.catch((error) => {
-			console.error(error);
-			process.exit(1);
-		})
-		.finally(async () => {
-			await prisma.$disconnect();
-		});
+  main()
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
 };
