1ee789c5ec6d55b15f9bed58becc137ecd3bf8f0
Rade58 <bajic.rade2@gmail.com>
2022-07-22 20:24:38 +0200

    🦉

--- cypress/e2e/06-sixt/2-nr-pok-spec.cy.js
+++ cypress/e2e/06-sixt/2-nr-pok-spec.cy.js
@@ -10,7 +10,22 @@ const pokemons = [
 describe('Pokemon search', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/pokemon-search');
+
+    cy.get('[data-test="search"]').as('search');
+    cy.get('[data-test="search-label"]').as('label');
+    // AN ALIAS FOR INTERCEPTING NETWORK REQUEST
+    // AS YOU CAN SE YOU CAN USE GLOB
+    cy.intercept('/pokemon-search/api?*').as('api');
   });
 
-  it('', () => {});
+  it('should call the API when the user types', () => {
+    cy.wait(1000);
+
+    cy.get('@search').type('charm', {
+      delay: 100,
+      timeout: 100
+    });
+    // I GUESS WE ARE WAITING ON THIS REQUEST
+    cy.wait('@api');
+  });
 });
