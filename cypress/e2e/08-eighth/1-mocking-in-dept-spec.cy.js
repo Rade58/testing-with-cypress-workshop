3711909c57d1bec8500535c09caa15cf22d2ae1a
Rade58 <bajic.rade2@gmail.com>
2022-07-23 17:43:24 +0200

    🦉

--- cypress/e2e/08-eighth/1-mocking-in-dept-spec.cy.js
+++ cypress/e2e/08-eighth/1-mocking-in-dept-spec.cy.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
+// I STARTED DOING THIS BUT STOPPED
+// THIS TEST ISN'T COMPLETED AND IT IS NOT RELEVANT
+// SINCE I AM USING SERVER SIDE RENDERING
+// I WANTED TO USE FIXTURES FOR THE API I'M FETCHING FROM
+// BUT THESE APS AR INSIDE lod FUNCTION OF THE PAGE
+
 const decodeToken = (token /* : string */) =>
   JSON.parse(Buffer.from(token, 'base64').toString('utf-8'));
 const encodeToken = (userData /* : {email:string; id:string} */) =>
@@ -22,17 +28,22 @@ describe('Signing in with a mocked data', () => {
     cy.setCookie('jwt', encodeToken(user));
 
     cy.intercept('GET', '/echo-chamber/api', { fixture: 'posts' }).as('postsApi');
-    cy.intercept('GET', '/echo-chamber/api/*', { fixture: 'post' }).as('postApi');
+    cy.intercept('GET', /\/echo-chamber\/api\/d+/, { fixture: 'post' }).as('postApi');
+    cy.visit('http://localhost:3000/echo-chamber/posts');
     // cy.intercept("GET", "/echo-chamber/api", {fixture: "posts"}).as("postsApi");
+
+    cy.get('[data-test="post-preview-list"] a').as('previews');
+  });
+
+  it('should render the posts from the API', () => {
+    cy.fixture('posts').then(({ posts }) => {
+      cy.get('@previews').should('have.length', posts.length);
+    });
   });
 
   it('', () => {
-    cy.visit('http://localhost:3000/echo-chamber/posts');
-
-    cy.wait('@postsApi');
-
-    cy.visit('http://localhost:3000/echo-chamber/posts/1');
-
-    cy.wait('@postApi');
+    // cy.wait('@postsApi');
+    // cy.visit('http://localhost:3000/echo-chamber/posts/607');
+    // cy.wait('@postApi');
   });
 });
